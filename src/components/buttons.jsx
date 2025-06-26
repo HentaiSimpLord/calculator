5b286defe163c7b7454ed11d6b8dcd8370db37f0
hentaisimplord <pierrefrancois1907@gmail.com>
2025-06-27 00:03:34 +0200

    0.2.1 handleClick

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -1,10 +1,12 @@
 import buttons from "../buttons";
 
 export const Buttons = () => {
+  const handleClick = () => {};
+
   return (
     <div id="buttons">
       {Object.entries(buttons).map(([key, obj]) => (
-        <button key={key} id={obj.id}>
+        <button key={key} id={obj.id} onClick={handleClick}>
           {obj.name}
         </button>
       ))}
